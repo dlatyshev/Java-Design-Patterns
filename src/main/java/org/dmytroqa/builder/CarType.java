a2b54e506d8cc60139e32c8355b7ba11992af7f2
dmytroqa <d_latyshev@ukr.net>
2021-10-22 15:50:36 +0300

    Builder pattern

---
+++ src/main/java/org/dmytroqa/builder/CarType.java
@@ -0,0 +1,5 @@
+package org.dmytroqa.builder;
+
+public enum CarType {
+    CITY_CAR, SPORTS_CAR, SUV
+}
---
+++ src/main/java/org/dmytroqa/builder/Transmission.java
@@ -0,0 +1,5 @@
+package org.dmytroqa.builder;
+
+public enum Transmission {
+    SINGLE_SPEED, MANUAL, AUTOMATIC, SEMI_AUTOMATIC
+}
